54e25e30a6e1c3bd2a1096f2ace88c6b12fde70a
elenaB1864 <90788027+elenaB1864@users.noreply.github.com>
2021-12-09 18:20:43 +0000

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -40,8 +40,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "c3ca6a249d521dfa006b0911272e88f0";
 
-let apiKey = "c3ca6a249d521dfa006b0911272e88f0";
-let city = "sydney";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
