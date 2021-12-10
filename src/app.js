9c68027d85066990123d10d7a65a9a1a197450d2
elenaB1864 <90788027+elenaB1864@users.noreply.github.com>
2021-12-10 13:29:00 +0000

    Added forecast JS

--- src/app.js
+++ src/app.js
@@ -20,6 +20,34 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = ` <div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `       
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+
+                <img
+                  src="http://openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18°C </span>
+                  <span class="weather-forecast-temperature-min">12°C</span>
+                </div>
+              
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + ` </div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -72,6 +100,7 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
