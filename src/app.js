41ff95d1a8e61ed44d542f46814cbf38dfcfe022
elenaB1864 <90788027+elenaB1864@users.noreply.github.com>
2021-12-10 13:49:32 +0000

    Added API call in JS

--- src/app.js
+++ src/app.js
@@ -22,8 +22,9 @@ function formatDate(timestamp) {
 }
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  let forecastHTML = ` <div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = ` <div class="row">`;
+
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -48,6 +49,11 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  let apiKey = "c3ca6a249d521dfa006b0911272e88f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -70,6 +76,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 function search(city) {
   let apiKey = "c3ca6a249d521dfa006b0911272e88f0";
@@ -100,7 +107,6 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
-displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
